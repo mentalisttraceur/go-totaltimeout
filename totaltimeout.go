a4ce8ee7757faa54c9530b7ae26fd24c88749ba6
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2019-11-27 09:07:15 +0000

    Added example to documentation

--- totaltimeout.go
+++ totaltimeout.go
@@ -19,6 +19,25 @@
 //
 // Usually idiomatic Go code uses contexts, timers, or deadlines
 // instead of timeouts - this helps dealing with code that doesn't.
+//
+// Example:
+//
+// Waiting in a "timed loop" for an API with retries (useful for APIs
+// that may have transient errors causing immediate error returns):
+//
+// 	timeout := totaltimeout.New(15 * time.Seconds)
+// 	retryDelay := 2 * time.Seconds
+// 	for timeLeft := timeout.TimeLeft(); timeLeft > 0 {
+// 		client := http.Client{Timeout: timeLeft}
+// 		response, err := client.get("https://flaky.api.example.com")
+// 		if err == nil && response.StatusCode == 200 {
+// 			break
+// 		}
+// 		if timeout.TimeLeft() < retryDelay {
+// 			break
+// 		}
+// 		time.Sleep(retryDelay)
+// 	}
 package totaltimeout
 
 import "time"
