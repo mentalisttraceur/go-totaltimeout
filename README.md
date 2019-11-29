3b7560fe34286a2ebfa1606e3b34d5a63f1c4846
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2019-11-29 04:47:34 +0000

    Split example and wording tweak

--- README.md
+++ README.md
@@ -7,10 +7,22 @@ already happened, to pass an adjusted timeout to the next step.
 Usually idiomatic Go code uses contexts, timers, or deadlines
 instead of timeouts - this helps dealing with code that doesn't.
 
-Example:
+Examples:
 
-Waiting in a "timed loop" for an API with retries (useful for APIs
-that may have transient errors causing immediate error returns):
+Waiting in a "timed loop" for an API with retries (useful
+for unreliable APIs that may either hang or need retries):
+
+	timeout := totaltimeout.New(15 * time.Seconds)
+	for timeLeft := timeout.TimeLeft(); timeLeft > 0 {
+		client := http.Client{Timeout: timeLeft}
+		response, err := client.get("https://flaky.api.example.com")
+		if err == nil && response.StatusCode == 200 {
+			break
+		}
+	}
+	// handle success or failure here, once all retries are done
+
+Same as above, but with a wait between retries:
 
 	timeout := totaltimeout.New(15 * time.Seconds)
 	retryDelay := 2 * time.Seconds
--- totaltimeout.go
+++ totaltimeout.go
@@ -20,10 +20,22 @@
 // Usually idiomatic Go code uses contexts, timers, or deadlines
 // instead of timeouts - this helps dealing with code that doesn't.
 //
-// Example:
+// Examples:
 //
-// Waiting in a "timed loop" for an API with retries (useful for APIs
-// that may have transient errors causing immediate error returns):
+// Waiting in a "timed loop" for an API with retries (useful
+// for unreliable APIs that may either hang or need retries):
+//
+// 	timeout := totaltimeout.New(15 * time.Seconds)
+// 	for timeLeft := timeout.TimeLeft(); timeLeft > 0 {
+// 		client := http.Client{Timeout: timeLeft}
+// 		response, err := client.get("https://flaky.api.example.com")
+// 		if err == nil && response.StatusCode == 200 {
+// 			break
+// 		}
+// 	}
+// 	// handle success or failure here, once all retries are done
+//
+// Same as above, but with a wait between retries:
 //
 // 	timeout := totaltimeout.New(15 * time.Seconds)
 // 	retryDelay := 2 * time.Seconds
